8a5175d045701ae277ac3b9043ace786fe3bbd30
thanmaimeher <103480409+thanmaimeher@users.noreply.github.com>
2024-11-26 19:00:20 +0530

    head first python 01

---
+++ .venv/pythonrummy_random.py
@@ -0,0 +1,12 @@
+import random
+suits = ["Clubs", "Spades", "Hearts", "Diamonds"]
+faces = ["Jack", "Queen", "King", "Ace"]
+numbered = [2, 3, 4, 5, 6, 7, 8, 9, 10]
+def draw():
+    the_suit = random.choice(suits)
+    the_card = random.choice(faces + numbered)
+    return the_card, "of", the_suit
+print(draw())
+print(draw())
+print(draw())
+
