9c3d9579f875183dd84ba4b599889cf7abe482f7
thanmaimeher <103480409+thanmaimeher@users.noreply.github.com>
2024-12-04 13:35:32 +0530

    head first python cards program

---
+++ .venv/Lib/learning_python/mathamaticalops.py
@@ -0,0 +1,25 @@
+print(1+2)#addition
+print(1-2)#substartion
+print(2*1)#multiplication
+print(6/2)#division is always float
+print(type(6/2))#3.0 <class 'float'>
+print(5/2)#2.5
+#to get only int from the division we have // operator
+print(6//2)#output 3 <class 'int'>
+print(5//2)#output is 2 <class 'int'>
+#so // always ignores decimel part
+print(2**3)# this is equal to 2 to the power of 3 output is 8
+print(3*3+3/3-3)#output is 7
+# # in above contest we use PEMDASLR ROLE to understand the priyority
+# # P: paranthosis
+# # E: EXPONENT (POWERS)
+# # M: MULTIPLICATION
+# # D: Division
+# # A: ADDITION
+# # S: SUBSTARTION
+# # LR: ORDER SHOULD BE FROM LEFT TO RIGHT
+# #SO in case of 3*3+3/3-3
+# step1: 9+3/3-3 M
+# step2: 9+1-3 D
+# step3: 10-3 A
+# step4: 7 S
