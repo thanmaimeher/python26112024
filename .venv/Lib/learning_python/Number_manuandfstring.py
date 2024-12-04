587320d3d532c83a914bf70279c7b0f6c4b6672c
thanmaimeher <103480409+thanmaimeher@users.noreply.github.com>
2024-12-04 13:34:40 +0530

    head first python cards program

---
+++ .venv/Lib/learning_python/Number_manuandfstring.py
@@ -0,0 +1,10 @@
+bmi = 200/1.64**2
+print(bmi)#output is 74.36049970255802
+#if we have only 5 digit plcae holder in our code to make it 5 digit we can use
+print(int(bmi))#output is 74 but it is flooring to int
+print(round(bmi))#will round it to nearest int
+print(round(5.1))#output 5
+print(round(5.6))#output is 6
+print(round(bmi,4))#output is 74.3605 rounds after 4 decimal digits
+print(round(9.199999,2))#output is 9.2
+
